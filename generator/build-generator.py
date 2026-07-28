2dcfef30fe088bc2f452fcaf2e8fdaff077900c7
KitchenBreeze <esmeraldalorenz216@gmail.com>
2026-07-28 16:55:30 +0200

    schritt8

--- generator/build-generator.py
+++ generator/build-generator.py
@@ -1,1 +1,16 @@
-print("KitchenBreeze Generator läuft erfolgreich")
+import re
+
+title = "Spaghetti aglio e olio"
+
+slug = title.lower()
+
+slug = slug.replace("ä", "ae")
+slug = slug.replace("ö", "oe")
+slug = slug.replace("ü", "ue")
+slug = slug.replace("ß", "ss")
+
+slug = re.sub(r"[^a-z0-9\s-]", "", slug)
+slug = re.sub(r"\s+", "-", slug)
+
+print("Titel:", title)
+print("Slug:", slug)

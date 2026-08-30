9c71f3c712c74bad95f9e235b8a6ffc65dd43923
KitchenBreeze <esmeraldalorenz216@gmail.com>
2026-08-30 19:38:02 +0200

    ok

--- recipes.js
+++ recipes.js
@@ -3921,7 +3921,7 @@ related: ["45", "7", "20"],
             text: "Rechts und links neben dem Teigling jeweils ein Küchentuch unter dem Backpapier aufrollen und als Stütze eng anlegen, sodass der Teigling beim Aufgehen nicht zur Seite läuft.",
             images: [
                 "images/ciabatta-formen1.jpeg",
-                "images/ciabatta-formen2.jpeg"
+                "images/ciabatta-formen2.jpeg",
             ]
         },
 
